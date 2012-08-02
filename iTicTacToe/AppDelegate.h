f52dec5ce39489ad5d9d67155f2d4f92b1324fc5
David Sedgewick <davidsed@telus.net>
2012-08-01 21:51:57 -0700

    Initial Commit

---
+++ iTicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTicTacToe
+//
+//  Created by David Sedgewick on 12-08-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
